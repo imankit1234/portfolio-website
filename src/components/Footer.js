6f39950d042a5ba413492d355fecca61694a1ded
Ankit kumar <nks823001@gmail.com>
2024-01-06 03:36:07 +0530

    gmail link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,38 @@ import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
+export const Footer = () => {import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon7 from "../assets/img/nav-icon7.svg";
+
 export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+            <a href="https://www.linkedin.com/in/imankit81/"><img src={navIcon1} alt="" /></a>
+            <a href="https://github.com/imankit1234"><img src={navIcon2} alt="" /></a>
+            <a href="https://twitter.com/imankit81"><img src={navIcon3} alt="" /></a>
+              <a href="nks823001@gmail.com"><img src={navIcon7} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
   return (
     <footer className="footer">
       <Container>
